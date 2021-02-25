6358af0337b9f8f20346d93621e32bcb44a1a9bd
Mat Jordan <mat@utk.edu>
2021-02-24 22:26:35 -0500

    Make nice with the linter.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('mirador-playground'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('mirador-playground')
+);
 
 module.hot.accept();
