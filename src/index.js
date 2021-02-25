7c9da79d7ed08faf46cfb6a55f14443ab2d9d393
Mat Jordan <mat@utk.edu>
2021-02-24 22:24:24 -0500

    Remove UTK stuff.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('utk-mirador'));
+ReactDOM.render(<App />, document.getElementById('mirador-playground'));
 
 module.hot.accept();
